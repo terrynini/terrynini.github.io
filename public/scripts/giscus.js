f9560480609aee27a2f27d8de27bc44414180584
terrynini38514 <terrynini38514@gmail.com>
2025-07-25 00:20:19 +0800

    Fix: script path

--- public/scripts/giscus.js
+++ public/scripts/giscus.js
@@ -2,7 +2,6 @@
 function setGiscusTheme(theme) {
     const giscusIframe = document.querySelector('iframe.giscus-frame');
     if (!giscusIframe) {
-        // If Giscus isn't loaded yet, retry after a short delay.
         setTimeout(() => setGiscusTheme(theme), 500);
         return;
     }
@@ -17,10 +16,8 @@ function getGiscusTheme() {
     return document.body.classList.contains('theme-light') ? 'light' : 'dark';
 }
 
-// Set the initial theme
 setGiscusTheme(getGiscusTheme());
 
-// Observe changes to the body's class attribute
 const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
         if (mutation.attributeName === 'class') {
